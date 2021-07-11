7e6b1a3c0e6f880ff1684fe8d13ec6644332f5a3
wanje125 <whdhkswp07@naver.com>
2021-07-11 08:00:00 +0000

    all

---
+++ sql_queries.py
@@ -0,0 +1,52 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+# CREATE TABLES
+
+songplay_table_create = """CREATE TABLE IF NOT EXISTS songplays (datetime timestamp, userId varchar, level varchar, song_id varchar, 
+artist_id varchar, session_id int, location varchar, user_agent varchar);"""
+
+user_table_create = """CREATE TABLE IF NOT EXISTS users(userId int, firstName varchar, lastName varchar, gender varchar, level varchar);"""
+
+song_table_create = """ CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar,
+                    artist_id varchar, year int, duration float); """
+
+artist_table_create = """ CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, 
+                    location varchar, latitude float, longitude float);"""
+
+time_table_create = """ CREATE TABLE IF NOT EXISTS time (datetime timestamp, hour int, day int,week_of_year int, month int, year int, weekday int);"""
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""INSERT INTO songplays (datetime, userId, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES(%s, %s, %s, %s, %s,%s,%s,%s)
+""")
+
+user_table_insert = ("""INSERT INTO users (userId, firstName, lastName, gender, level) VALUES (%s, %s, %s, %s, %s)
+""")
+
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year ,duration)
+                 VALUES (%s, %s, %s, %s, %s)""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+                 VALUES (%s, %s, %s, %s, %s)
+""")
+
+
+time_table_insert = ("""INSERT INTO time (datetime, hour, day, week_of_year, month, year, weekday) VALUES(%s, %s, %s, %s,%s, %s,%s)
+""")
+
+# FIND SONGS
+
+song_select = ("""SELECT s.artist_id, s.song_id FROM artists as a JOIN songs as s ON  a.artist_id = s.artist_id 
+WHERE s.title=%s and a.name=%s and s.duration=%s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]

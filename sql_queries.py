83daca4baad9516f95eb0c307b4d49e782574a3e
wanje125 <whdhkswp07@naver.com>
2021-07-22 03:07:29 +0000

    change1

--- sql_queries.py
+++ sql_queries.py
@@ -13,7 +13,7 @@ artist_id varchar, session_id int, location varchar, user_agent varchar );"""
 user_table_create = """CREATE TABLE IF NOT EXISTS users(userId int PRIMARY KEY, firstName varchar, lastName varchar, gender varchar, level varchar);"""
 
 song_table_create = """ CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar,
-                    artist_id varchar NOT NULL REFERENCES artists, year int, duration float); """
+                    artist_id varchar NOT NULL, year int, duration float); """
 
 artist_table_create = """ CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, 
                     location varchar, latitude float, longitude float);"""
@@ -23,9 +23,10 @@ time_table_create = """ CREATE TABLE IF NOT EXISTS time (datetime timestamp PRIM
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (datetime, userId, level, song_id, artist_id, session_id, location, user_agent) 
-VALUES(%s, %s, %s, %s, %s,%s,%s,%s);""")
+VALUES(%s, %s, %s, %s, %s,%s,%s,%s) ;""")
 
-user_table_insert = ("""INSERT INTO users (userId, firstName, lastName, gender, level) VALUES (%s, %s, %s, %s, %s) ;""")
+user_table_insert = ("""INSERT INTO users (userId, firstName, lastName, gender, level) VALUES (%s, %s, %s, %s, %s) 
+                ON CONFLICT (userId) DO NOTHING;""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year ,duration)
                  VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;""")
@@ -34,7 +35,8 @@ artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitu
                  VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;""")
 
 
-time_table_insert = ("""INSERT INTO time (datetime, hour, day, week_of_year, month, year, weekday) VALUES(%s, %s, %s, %s,%s, %s,%s);""")
+time_table_insert = ("""INSERT INTO time (datetime, hour, day, week_of_year, month, year, weekday) VALUES(%s, %s, %s, %s,%s, %s,%s)
+                    ON CONFLICT (datetime) DO NOTHING;""")
 
 # FIND SONGS
 
@@ -43,5 +45,5 @@ WHERE s.title=%s and a.name=%s and s.duration=%s;""")
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [songplay_table_create, user_table_create,  song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
